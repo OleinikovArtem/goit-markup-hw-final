6a162d63af32a5d4aefbb1ea6bdb278b00125620
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2024-07-27 20:49:39 +0300

    remove console.log

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ const $mobileNavModal = document.getElementById('mobile-nav-modal')
 const $mobileNavClose = document.getElementById('mobile-nav-close')
 const $mobileNavOpen = document.getElementById('mobile-nav-open')
 
-console.log('mobileNavModal', $mobileNavModal);
-console.log('mobileNavClose', $mobileNavClose);
-console.log('mobileNavOpen', $mobileNavOpen);
-
 const OPEN_CSS_CLASS = 'is-open'
 
 $mobileNavOpen.onclick = () => $mobileNavModal.classList.add(OPEN_CSS_CLASS)
