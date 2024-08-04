e4eed7019ab6cb72e988c86f02ca657d98a1c249
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2024-08-04 16:58:48 +0300

    add js for mobile menu

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 /* Header Navigation */
-const $mobileNavModal = document.getElementById('mobile-nav-modal')
-const $mobileNavClose = document.getElementById('mobile-nav-close')
-const $mobileNavOpen = document.getElementById('mobile-nav-open')
+const $mobileNavModal = document.getElementById('mobile-nav-modal');
+const $mobileNavClose = document.getElementById('mobile-nav-close');
+const $mobileNavOpen = document.getElementById('mobile-nav-open');
+const $mobileNavLinks = document.querySelectorAll('.nav-mobile .nav-item');
 
-const OPEN_CSS_CLASS = 'is-open'
+const OPEN_CSS_CLASS = 'is-open';
 
-$mobileNavOpen.onclick = () => $mobileNavModal.classList.add(OPEN_CSS_CLASS)
-$mobileNavClose.onclick = () => $mobileNavModal.classList.remove(OPEN_CSS_CLASS)
+const onCloseMobileNavMenu = () => $mobileNavModal.classList.remove(OPEN_CSS_CLASS);
+
+$mobileNavOpen.onclick = () => $mobileNavModal.classList.add(OPEN_CSS_CLASS);
+$mobileNavClose.onclick = onCloseMobileNavMenu;
+
+$mobileNavLinks.forEach(element => {
+    element.onclick = onCloseMobileNavMenu;
+});
