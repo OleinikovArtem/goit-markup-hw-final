fe50aaa521ed2e5ac624cbcbc298dc62d5482f33
Volodymyr Kamyshanov <volodymyr.kamyshanov@gmail.com>
2024-08-04 17:02:30 +0300

    Close menu on link click

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 const $mobileNavModal = document.getElementById('mobile-nav-modal')
 const $mobileNavClose = document.getElementById('mobile-nav-close')
 const $mobileNavOpen = document.getElementById('mobile-nav-open')
+const $mobileNavLinks = document.getElementsByClassName('nav-link')
 
 const OPEN_CSS_CLASS = 'is-open'
 
 $mobileNavOpen.onclick = () => $mobileNavModal.classList.add(OPEN_CSS_CLASS)
 $mobileNavClose.onclick = () => $mobileNavModal.classList.remove(OPEN_CSS_CLASS)
+
+for (const element of $mobileNavLinks) {
+    element.onclick = () => $mobileNavModal.classList.remove(OPEN_CSS_CLASS)
+}
